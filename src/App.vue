a628000d817a477366bf9075f2597a38dc8b30b8
jokest16 <jokest16@gmail.com>
2017-04-26 00:52:20 +0300

    master: init commit

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app" class="app">
+    <div class="wrapper">
+      <NaviBar></NaviBar>
+      <AsideBar>svsd</AsideBar>
+      <Slider></Slider>
+      <ContentWrap></ContentWrap>
+      <Modal></Modal>
+    </div>
+  </div>
+</template>
+
+<script>
+import NaviBar from 'NaviBar.vue'
+import Slider from 'Slider.vue'
+import ContentWrap from 'ContentWrap.vue'
+import Modal from './components/Modal.vue'
+import store from './vuex/store.js'
+import AsideBar from 'AsideBar.vue'
+
+export default {
+  name: 'app',
+  created () {
+  },
+  components: {
+    AsideBar,
+    NaviBar,
+    Slider,
+    ContentWrap,
+    Modal
+  },
+  store
+}
+</script>
+
+<style>
+
+  .app > .wrapper {
+    overflow-y: hidden;
+  }
+
+</style>
---
+++ src/AsideBar.vue
@@ -0,0 +1,75 @@
+<template>
+    <aside class="asside-bar" v-show="showAsidePanels">
+        <div class="content" v-bind:class="{ '-fifty': showFiftyAsidePanels,'-left': showAdditionalPanel }">1</div>
+        <div v-bind:class="{ 'content': showCentredPanel,'-top': showFiftyAsidePanels }">1</div>
+        <div class="content" v-bind:class="{ '-fifty': showFiftyAsidePanels,'-right': showAdditionalPanel }">3</div>
+        <button @click="showPanel">qwe</button>
+    </aside>
+</template>
+
+<script>
+
+  export default {
+    name: 'AsideBar',
+    data () {
+      return {
+        showAsidePanels: true,
+        showCentredPanel: false,
+        showFiftyAsidePanels: true,
+        showAdditionalPanel: false
+      }
+    },
+    methods: {
+      showPanel () {
+        if (!this.showCentredPanel && this.showAdditionalPanel) {
+          this.showCentredPanel = false
+          this.showFiftyAsidePanels = true
+        } else {
+          this.showCentredPanel = true
+          this.showFiftyAsidePanels = false
+        }
+      }
+    }
+  }
+
+</script>
+
+<style>
+    .asside-bar {
+        position: fixed;
+        right: 0;
+        left: 0;
+        bottom: 0;
+        top: 0;
+        z-index: 1100;
+        background-color: rgba(0, 0, 0, .7);
+        display: flex;
+        justify-content: space-around;
+        padding: 10px;
+    }
+
+    .asside-bar > .content {
+        display: inline-block;
+        background-color: #ecf0f5;
+        min-width: 32%;
+        overflow: hidden;
+        transition: transform .3s ease, min-width .3s ease;
+    }
+
+    .asside-bar > .content.-fifty {
+        min-width: 48%;
+    }
+
+    .asside-bar > .content.-left {
+        transform: translateX(-110%);
+    }
+
+    .asside-bar > .content.-right {
+        transform: translateX(110%);
+    }
+
+    .asside-bar > .content.-top {
+        transform: translateY(110%);
+    }
+
+</style>
